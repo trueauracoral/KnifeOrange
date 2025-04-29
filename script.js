7dd2b233bfb64fc210cf6376227d542a5873de3a
ZortaZert <trueauracoral@fedora.email>
2025-04-29 11:44:26 -0500

    Figured out rotation better and I also am working on te physics WIP

--- script.js
+++ script.js
@@ -3,25 +3,46 @@ let rotationCoords = {x: 307, y: 314};
 let knifeWidth = 27
 let knifeHeight = 137
 
+function degreesToRadians(degrees) {
+    return degrees * (Math.PI / 180);
+}
+
 class knife { 
     constructor(pos) {
+        this.gravity = 9.8;
+        this.gravityForce =
         this.pos = pos;
+        this.angle = 90;
+        this.force = 0;
+        this.torque = this.force * this.distance * Math.sin(degreesToRadians(this.angle));
+        this.endingCoords = {};
+        this.distance = knifeHeight;
+        this.mass = 5;
+        this.momentOfInertia = this.mass * Math.pow(this.distance, 2);
+        this.angularAcceleration = this.torque/ this.momentOfInertia;
+        this.angularVelocity = 0;
     }
 
     update() {
         //this.pos.y += this.velocity.y;
+        this.endingCoords = {
+            x: this.pos.x + knifeHeight * Math.cos(degreesToRadians(this.angle)),
+            y: this.pos.y + knifeHeight * Math.sin(degreesToRadians(this.angle))
+        }
+        this.torque = this.force * this.distance * Math.sin(degreesToRadians(this.angle));
+        this.angularVelocity +=this.angularAcceleration;
+        this.angle += this.angularVelocity;
     };
 
     draw() {
         const rotationPointX = rotationCoords.x;
         const rotationPointY = rotationCoords.y;
-        const angleInDegrees = 45;
-        const angleInRadians = angleInDegrees * Math.PI / 180;
+        const angleInRadians = this.angle * Math.PI/180;
       
         ctx.save();
         ctx.translate(rotationPointX, rotationPointY);
         ctx.rotate(angleInRadians);
-        ctx.drawImage(knifeIMG, -knifeWidth/2, -knifeHeight/2);
+        ctx.drawImage(knifeIMG, 14+-knifeWidth/2, -8);
         ctx.translate(-this.pos.x, -this.pos.y);
 
         ctx.restore();
@@ -75,6 +96,8 @@ function gameDraw() {
     Knife.draw();
     ctx.fillStyle = "red";
     ctx.fillRect(rotationCoords.x, rotationCoords.y, 10, 10)
+    ctx.fillRect(Knife.endingCoords.x, Knife.endingCoords.y, 10, 10)
+
 }
 
 function gameLoop() {
@@ -86,4 +109,9 @@ function gameLoop() {
     gameDraw()
     
 }
+
+canvas.addEventListener('pointerdown', (event) => {
+    Knife.force += 1;
+    console.log(Knife.force);
+  });
 gameLoop();
